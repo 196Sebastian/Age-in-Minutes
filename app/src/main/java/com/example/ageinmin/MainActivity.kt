1828dc9f2691106fd151f5600a333c9638c0394f
Sebastian Correa <correa.sebas6@gmail.com>
2021-07-08 12:31:37 -0500

    Removed the main function, added the missing plugins & added a click a date picker

--- app/src/main/java/com/example/ageinmin/MainActivity.kt
+++ app/src/main/java/com/example/ageinmin/MainActivity.kt
@@ -1,15 +1,34 @@
 package com.example.ageinmin
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
-fun main(){
-
-}
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnDatePicker.setOnClickListener {view ->
+            clickDatePicker(view)
+        }
+    }
+
+    fun clickDatePicker(view: View){
+
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+        DatePickerDialog(this, DatePickerDialog.OnDateSetListener {
+                view, year, month, dayOfMonth ->
+            Toast.makeText(this, "Date picker Works", Toast.LENGTH_LONG).show()
+        } ,year,month,day).show()
+
     }
 }
