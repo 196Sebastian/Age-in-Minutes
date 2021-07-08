0a912015e23a7d30276a9167ac22975ad7d97788
Sebastian Correa <correa.sebas6@gmail.com>
2021-07-08 09:45:30 -0500

    Added main function

--- app/src/main/java/com/example/ageinmin/MainActivity.kt
+++ app/src/main/java/com/example/ageinmin/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.ageinmin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+fun main(){
+
+}
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
